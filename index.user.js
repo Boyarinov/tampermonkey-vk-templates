4684e85bbedecd88065661b4c1d970c271d8a424
Svotin <dogweb2023@gmail.com>
2024-08-18 15:22:51 +0300

    fix: update version

--- index.user.js
+++ index.user.js
@@ -2,7 +2,7 @@
 // @name         Support Templates
 // @description  Templates for VK support
 // @namespace    http://tampermonkey.net/
-// @version      1.3
+// @version      1.4
 // @author       Svotin
 // @match        https://vk.com/*
 // @icon         https://www.google.com/s2/favicons?domain=vk.com
